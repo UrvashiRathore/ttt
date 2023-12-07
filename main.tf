26fad5e63c7f3d5a0f00bc3cdb19da10f8e7cc2c
UrvashiRathore <rathore.urvashi21@gmail.com>
2023-12-07 10:01:25 +0530

    2nd

--- main.tf
+++ main.tf
@@ -4,4 +4,11 @@ tags = {
  Names = "Major_Proj"
 }
 }
-#checking diff
+resource "aws_subnet" "public-subnet" {
+  vpc_id = aws_vpc.s7.id
+  cidr_block = "10.0.7.0/24"
+  availability_zone = "ap-south-1a"
+  tags = {
+    Name = "Public subnet "
+  }
+}
